d3dad90532e1307af962cba745ed1d243590121e
theoliverlear <sigwartholiver2@gmail.com>
2025-11-20 16:21:34 -0600

    Refactor AudioMeter.cpp to remove unnecessary `this->` qualifiers on static methods

--- source/components/AudioMeter.cpp
+++ source/components/AudioMeter.cpp
@@ -20,19 +20,18 @@ void AudioMeter::paint(Graphics& graphics)
     const MeterVisualStyle& style = defaultMeterStyle();
     const float cornerRadius = style.cornerRadius;
     const float innerPadding = style.innerPadding;
-    const Rectangle<float> columnBounds = this->computeColumnBounds(componentArea, this->leftGutterWidth, this->showTicks);
-    this->drawBackground(graphics, columnBounds, style, cornerRadius);
+    const Rectangle<float> columnBounds = computeColumnBounds(componentArea, this->leftGutterWidth, this->showTicks);
+    drawBackground(graphics, columnBounds, style, cornerRadius);
     const Rectangle<float> innerRect = computeInnerRect(columnBounds, innerPadding);
     const float currentLevel = jlimit(0.0f, 1.0f, this->levelPtr ? *this->levelPtr : 0.0f);
     const float levelNormalised = computeLevelNormalized(currentLevel, this->minDb, this->maxDb);
-    this->drawFilledBar(graphics, innerRect, levelNormalised, cornerRadius, style);
-    this->drawPeakHoldMarker(graphics, innerRect, this->peakHoldLevel);
-    this->drawClipLed(graphics, columnBounds, this->clipHoldFrames > 0, style.ledSize);
+    drawFilledBar(graphics, innerRect, levelNormalised, cornerRadius, style);
+    drawPeakHoldMarker(graphics, innerRect, this->peakHoldLevel);
+    drawClipLed(graphics, columnBounds, this->clipHoldFrames > 0, style.ledSize);
     if (this->showTicks)
     {
-        this->drawDbTicksAndLabels(graphics, componentArea, innerRect,
+        drawDbTicksAndLabels(graphics, componentArea, innerRect,
             this->minDb, this->maxDb, this->leftGutterWidth);
     }
-
-    this->drawTopLabels(graphics, columnBounds, this->label, currentLevel, this->minDb);
+    drawTopLabels(graphics, columnBounds, this->label, currentLevel, this->minDb);
 }
