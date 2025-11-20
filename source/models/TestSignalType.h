fc01b4cc0dc27cbb8625c5482f45d84513771bac
theoliverlear <sigwartholiver2@gmail.com>
2025-11-20 16:22:53 -0600

    Add TestSignalType.h to define an enumeration for test signal types including sine, noise, and sweep types

---
+++ source/models/TestSignalType.h
@@ -0,0 +1,18 @@
+//
+// Created by olive on 11/19/2025.
+//
+
+#ifndef TRINITY_TESTSIGNALTYPE_H
+#define TRINITY_TESTSIGNALTYPE_H
+
+enum TestSignalType
+{
+    kOff = 0,
+    kSine17k = 1,
+    kSine19k = 2,
+    kWhiteNoise = 3,
+    kPinkNoise = 4,
+    kLogSweep = 5
+};
+
+#endif //TRINITY_TESTSIGNALTYPE_H
